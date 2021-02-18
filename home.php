190f0dc8a34c538e79bbc0ef6a3e89670ac63a16
Emmanuel Béziat <contact@emmanuelbeziat.com>
2021-02-18 14:33:14 +0100

    infos souris

--- home.php
+++ home.php
@@ -18,20 +18,19 @@
 <?php if ($loop->have_posts()) : while ($loop->have_posts()) : $loop->the_post(); ?>
 <a href="<?php echo get_permalink() ?>" <?php post_class('') ?> id="post-<?php the_ID(); ?>">
 	<h2><?= get_the_title() ?></h2>
-	<?php debug(get_field('presentation')) ?>
 	<ul>
-		<li>Photo: <code><?= get_field('photo') ?></code></li>
-		<li>ID: <code><?= get_field('identifiant') ?></code></li>
-		<li>Prénom: <code><?= get_field('prenom') ?> (signification)</code></li>
-		<li>Sexe: <code><?= get_field('sexe') ?></code></li>
-		<li>Origines: <code><?= get_field('provenance') ?></code></li>
-		<li>Naissance: <code><?= get_field('date') ?></code></li>
-		<li>Couleur: <code><?= get_field('couleur') ?></code></li>
-		<li>Type de poil: <code><?= get_field('poil') ?></code></li>
-		<li>Poids: <code><?= get_field('poids') ?></code></li>
-		<li>Status: <code><?= get_field('status') ?></code></li>
-		<li>Vente: <code><?= get_field('vente') ?></code></li>
-		<li>Génotype: <code><?= get_field('genotype') ?></code></li>
+		<li>Photo: <code><?= get_field('presentation')['photo'] ?></code></li>
+		<li>ID: <code><?= get_field('presentation')['identifiant'] ?></code></li>
+		<li>Prénom: <code><?= get_field('presentation')['prenom'] ?> (signification)</code></li>
+		<li>Sexe: <code><?= get_field('presentation')['sexe'] ?></code></li>
+		<li>Origines: <code><?= get_field('origines')['provenance'] ?></code></li>
+		<li>Naissance: <code><?= get_field('origines')['date'] ?></code></li>
+		<li>Couleur: <code><?= get_field('apparence')['couleur'] ?></code></li>
+		<li>Type de poil: <code><?= get_field('apparence')['poil'] ?></code></li>
+		<li>Poids: <code><?= get_field('apparence')['poids'] ?></code></li>
+		<li>Status: <code><?= get_field('autres')['status'] ?></code></li>
+		<li>Vente: <code><?= get_field('autres')['vente'] ?></code></li>
+		<li>Génotype: <code><?= get_field('autres')['genotype'] ?></code></li>
 	</ul>
 </a>
 <?php endwhile; endif; ?>
