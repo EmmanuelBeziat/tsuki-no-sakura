70dbe520841a3a1e4cde870d48f3ab60d2c69bca
Emmanuel Béziat <contact@emmanuelbeziat.com>
2021-02-18 14:27:10 +0100

    home

--- home.php
+++ home.php
@@ -16,7 +16,7 @@
 	'posts_per_page' => -1
 ]); ?>
 <?php if ($loop->have_posts()) : while ($loop->have_posts()) : $post = $loop->the_post(); ?>
-<?php debug($post) ?>
+<?php // debug($post) ?>
 <a href="<?php echo get_permalink() ?>" <?php post_class('') ?> id="post-<?php the_ID(); ?>">
 	<h2><?= get_the_title() ?></h2>
 	<ul>
