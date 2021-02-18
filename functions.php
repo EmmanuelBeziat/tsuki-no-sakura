a0505f3f6d960c564526473f3f136c4b82bc5798
Emmanuel Béziat <contact@emmanuelbeziat.com>
2021-02-18 14:23:34 +0100

    debug

--- functions.php
+++ functions.php
@@ -352,3 +352,24 @@ remove_action('wp_head', 'wlwmanifest_link');
 function debug ($value) { ?>
 <pre style="margin: 1rem 0; padding: 1rem;background: #e4e4e4;border:1px solid: #ccc;"><code><?php var_dump($value); ?></code></pre>
 <?php }
+
+/**
+ * Gets unique ID.
+ *
+ * This is a PHP implementation of Underscore's uniqueId method. A static variable
+ * contains an integer that is incremented with each call. This number is returned
+ * with the optional prefix. As such the returned value is not universally unique,
+ * but it is unique across the life of the PHP process.
+ *
+ * @see wp_unique_id() Themes requiring WordPress 5.0.3 and greater should use this instead.
+ *
+ * @param string $prefix Prefix for the returned ID.
+ * @return string Unique ID.
+ */
+function sakura_unique_id($prefix = '') {
+	static $id_counter = 0;
+	if (function_exists('wp_unique_id')) {
+		return wp_unique_id($prefix);
+	}
+	return $prefix . (string) ++$id_counter;
+}
