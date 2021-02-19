1314d1ce58fa76c4c01ae7cf73c5e92308e2d691
Emmanuel Béziat <contact@emmanuelbeziat.com>
2021-02-19 04:52:33 +0100

    paths

--- inc/assets.php
+++ inc/assets.php
@@ -3,7 +3,7 @@
 function sakura_register_styles() {
 	$theme_version = wp_get_theme()->get('Version');
 
-	wp_enqueue_style('sakura-style', get_template_directory_uri() . '/dist/main.min.css', [], $theme_version);
+	wp_enqueue_style('sakura-style', get_template_directory_uri() . '/assets/css/main.min.css', [], $theme_version);
 }
 
 add_action('wp_enqueue_scripts', 'sakura_register_styles');
@@ -18,7 +18,8 @@ function sakura_register_scripts() {
 		wp_enqueue_script('comment-reply');
 	}
 
-	wp_enqueue_script('sakura-js', get_template_directory_uri() . '/dist/main.min.js', [], $theme_version, false);
+	// wp_enqueue_script('sakura-js', get_template_directory_uri() . '/assets/css/main.min.js', [], $theme_version, false);
+	wp_enqueue_script('sakura-js', get_template_directory_uri() . '/assets/css/main.min.js', [], $theme_version, false);
 	wp_script_add_data('sakura-js', 'defer', true);
 	wp_script_add_data('sakura-js', 'type', 'module');
 }
