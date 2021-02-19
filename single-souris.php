e0fc5ee56ba7bbf50bdf5828de396d12d075ba0e
Emmanuel Béziat <contact@emmanuelbeziat.com>
2021-02-19 04:03:16 +0100

    test

--- single-souris.php
+++ single-souris.php
@@ -11,17 +11,13 @@
 
 get_header();
 
-$args = [
-	'post_type' => 'souris',
-	'posts_per_page' => -1
-];
-
-$loop = new WP_Query($args);
-
 /* Start the Loop */
-while ($loop->have_posts()) : $loop->the_post();
-	debug($loop->the_post());
+if (have_posts()):
+	echo 'have posts';
+
+while (have_posts()) : the_post();
 	get_template_part('template-parts/souris.php');
 endwhile;
 
+endif;
 get_footer();
